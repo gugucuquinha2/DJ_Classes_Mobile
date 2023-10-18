34d31c1c68cc8a9be022c0f57b34cab7952842df
gugucuquinha <gcgustavocarneiro10@gmail.com>
2023-10-18 08:51:41 +0100

    Added Swipe + Drag input examples

---
+++ Assets/Class_UnityInput/01_Scripts/UnityInput_DragExample.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityInput_DragExample : MonoBehaviour
+{
+
+    private Vector3 offset;
+    private Vector3 screenObjectPoint;
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // cache the camera object for optimization
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            // when the touch starts
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                // convert the object's position into the screen position
+                screenObjectPoint = cam.WorldToScreenPoint(transform.position);
+
+                // get the touch position, but considering the actual object's depth (Z axis - we're in 3D)
+                Vector3 touchPos = new Vector3(Input.touches[0].position.x, Input.touches[0].position.y, screenObjectPoint.z);
+
+                // calculate the offset between the object position and the actual mouse position (in world space)
+                // the purpose of this is to make sure the object stays in position no matter where you grab it from (otherwise it will jump to your touch location)
+                offset = transform.position - cam.ScreenToWorldPoint(touchPos);
+            }
+            // when we're dragging the finger around the screen
+            else if (Input.touches[0].phase == TouchPhase.Moved)
+            {
+                // get the touch position in world space
+                Vector3 touchPos = new Vector3(Input.touches[0].position.x, Input.touches[0].position.y, screenObjectPoint.z);
+
+                // set the position of the object
+                // if you want the object to snap to the touch position, just remove the "offset" calculation
+                transform.position = cam.ScreenToWorldPoint(touchPos) + offset;
+            }
+        }
+    }
+
+}
---
+++ Assets/Class_UnityInput/01_Scripts/UnityInput_SwipeExample.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityInput_SwipeExample : MonoBehaviour
+{
+    // movement variables
+    public float speed;
+
+    private float  touchTimeStart;
+    private float touchTimeEnd;
+    private float timeDelta;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    private Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if there's tocuhes...
+        if(Input.touchCount > 0)
+        {
+            // ... and the touch just began
+            if(Input.touches[0].phase == TouchPhase.Began)
+            {
+                // record its time and position
+                touchTimeStart = Time.time;
+                startPos = Input.touches[0].position;
+            }
+
+            // ... if the touch ended
+            if (Input.touches[0].phase == TouchPhase.Ended)
+            {
+                // record the time and position
+                touchTimeEnd = Time.time;
+                endPos = Input.touches[0].position;
+
+                // calculate the swipe direction and how fast is the swipe
+                timeDelta = touchTimeEnd - touchTimeStart;
+                direction = endPos - startPos;
+               
+            }
+        }
+
+        // use that direction and speed to specify where to move the cube and how fast
+        transform.position += direction.normalized * (speed / timeDelta) * Time.deltaTime;
+
+    }
+
+}
